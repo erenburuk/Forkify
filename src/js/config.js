108f315e3de49ad7153065fa21328c2540cbf501
erenburuk <erenburukk@gmail.com>
2023-02-28 17:40:00 +0300

    Uploading a new recipe v2

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '7c00f3e8-cc7d-46cb-bbe5-79c53d85a308';
+export const MODAL_CLOSE_SEC = 2.5;
